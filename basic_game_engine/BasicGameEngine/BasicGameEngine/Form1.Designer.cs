46412e3294e22362dd9d0deee3eb5686c8a1d4b3
Malcolm Greene <Fantym420@GMail.Com>
2017-02-15 00:50:44 -0800

    Started making a basic game engine
    I created a basic game loop, next i'll create a gameObject class and
    move all relevant code in to it.

---
+++ basic_game_engine/BasicGameEngine/BasicGameEngine/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace BasicGameEngine
+{
+    partial class gameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // gameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 562);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "gameForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Basic Game Engine";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.gameForm_FormClosing);
+            this.Load += new System.EventHandler(this.gameForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.gameForm_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gameForm_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gameForm_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+    }
+}
+
---
+++ basic_game_engine/BasicGameEngine/BasicGameEngine/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicGameEngine
+{
+  
+    public partial class gameForm : Form
+    {
+
+        [DllImport("kernel32.dll")]
+        private static extern long GetTickCount();
+
+        private System.Drawing.Bitmap screenBuffer;
+        private System.Drawing.Bitmap[] gameGraphics;
+
+        private System.Drawing.PointF screenCenter;
+        private System.Drawing.PointF cameraPos;
+        private System.Drawing.PointF shipPos;
+        private System.Drawing.PointF shipVel;
+
+        private float gameTime = 0.0f;
+        private float timePool = 0.0f;
+        private float timeStep = 1.0f / 60.0f;
+        private long lastTickCount = GetTickCount();
+        private long curTickCount = 0;
+        private float timeDelta = 0.0f;
+        
+        private bool gameRunning = true;
+
+        private bool upPressed = false;
+        private bool downPressed = false;
+        private bool leftPressed = false;
+        private bool rightPressed = false;
+        
+        private Graphics graphicsObject;
+
+        public gameForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void gameForm_Load(object sender, EventArgs e)
+        {
+            graphicsObject = this.CreateGraphics();
+            gameGraphics = new System.Drawing.Bitmap[1];
+            gameGraphics[0] = (Bitmap) Image.FromFile(@"Images\ship.png");
+
+            screenBuffer = Image.FromHbitmap(new Bitmap(this.Width, this.Height).GetHbitmap());
+
+            shipPos = new System.Drawing.PointF(0.0f, 0.0f);
+            shipVel = new System.Drawing.PointF(0.0f, 0.0f);
+            cameraPos = new System.Drawing.PointF(0.0f, 0.0f);
+            screenCenter = new System.Drawing.PointF(this.Width * 0.5f, this.Height * 0.5f);
+
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint |
+                           ControlStyles.UserPaint |
+                           ControlStyles.DoubleBuffer,
+                           true);
+        }
+
+        public void gameLoop()
+        {
+            while (gameRunning)
+            {
+
+                curTickCount = GetTickCount();
+                timeDelta = (curTickCount - lastTickCount) * 0.001f;
+                lastTickCount = curTickCount;
+
+                timePool += timeDelta;
+
+                while (timePool >= timeStep)
+                {
+
+                    gameTime += timeStep;
+                    timePool -= timeStep;
+
+                    gameUpdate();
+                    cameraPos = shipPos;
+                    gameRender(Graphics.FromImage(screenBuffer));
+
+                }
+
+
+                graphicsObject.DrawImage(screenBuffer, new Point(0,0));
+
+                Application.DoEvents();
+
+
+            }
+        }
+
+        private void gameUpdate()
+        {
+
+            if (upPressed) shipVel.Y -= 30 * timeStep;
+            if (downPressed) shipVel.Y += 30 * timeStep;
+            if (leftPressed) shipVel.X -= 30 * timeStep;
+            if (rightPressed) shipVel.X += 30 * timeStep;
+            
+            shipPos.X += (shipVel.X * timeStep);
+            shipPos.Y += (shipVel.Y * timeStep);
+            
+        }
+
+        private void gameRender(Graphics g)
+        {
+            
+            g.Clear(Color.Black);
+
+            System.Drawing.PointF drawPos = new PointF();
+
+            drawPos.X = screenCenter.X + cameraPos.X - (shipPos.X + (gameGraphics[0].Width * 0.5f));
+            drawPos.Y = screenCenter.Y + cameraPos.Y - (shipPos.Y + (gameGraphics[0].Height * 0.5f));
+
+            g.DrawImage(gameGraphics[0], drawPos);
+
+
+        }
+
+        private void gameForm_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            System.Diagnostics.Debug.Write(e.KeyCode);
+
+            if (e.KeyCode == Keys.Up)
+            {
+                upPressed = true;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                downPressed = true;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                leftPressed = true;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                rightPressed = true;
+            }
+        }
+
+        private void gameForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                upPressed = false;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                downPressed = false;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                leftPressed = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                rightPressed = false;
+            }
+        }
+
+        private void gameForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            gameRunning = false;
+        }
+
+        private void gameForm_Paint(object sender, PaintEventArgs e)
+        {
+            
+            //gameRender(e.Graphics);
+            //System.Diagnostics.Debug.Print("painting");
+        }
+    }
+}
